0b7ba382de73ded5e197a5ff750d83d9fb93cdf1
awj <shahd.ibrahim.akra@gmail.com>
2019-04-20 17:26:43 +0300

    Initial commit

---
+++ app/src/main/java/com/example/assignment2/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.assignment2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.AdapterView;
+import android.view.View;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.list);
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent,
+                                    View view,
+                                    int position,
+                                    long id) {
+                if(position == 0){
+                    Intent intent = new Intent(MainActivity.this, Duties.class);
+                    startActivity(intent);
+                }
+                else if(position == 1){
+                    Intent intent = new Intent(MainActivity.this, ToDo.class);
+                    startActivity(intent);
+                }
+                else if(position == 2){
+                    Intent intent = new Intent(MainActivity.this, Progress.class);
+                    startActivity(intent);
+                }
+            }
+        };
+
+        listView.setOnItemClickListener(itemClickListener);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/assignment2/ToDo.java
@@ -0,0 +1,37 @@
+package com.example.assignment2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class ToDo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to_do);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id==R.id.menu_delet){
+
+        }
+        else if (id==R.id.menu_update){
+
+        }
+
+        return true;
+    }
+}
